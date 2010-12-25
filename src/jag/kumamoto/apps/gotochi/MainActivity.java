dacbf973408198e371175f273af985699cfd1544
aharisu <foo.yobina@gmail.com>
2010-12-26 03:53:24 +0900

    genフォルダが存在しない問題の修正
    メインアクティビティを修正して、野望を表示するように機能を追加

--- src/jag/kumamoto/apps/gotochi/MainActivity.java
+++ src/jag/kumamoto/apps/gotochi/MainActivity.java
@@ -2,12 +2,33 @@ package jag.kumamoto.apps.gotochi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.main);
+        
+        setupViewEventHandler();
+    }
+    
+    /**
+     * ビューのイベントハンドラの設定を行う
+     */
+    private void setupViewEventHandler() {
+    	
+        findViewById(R.id_main.btnAmbition).setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick(View v) {
+				
+				//野望を表示するよ!
+				Toast.makeText(MainActivity.this, getResources().getString(R.string.ambition), 
+						Toast.LENGTH_LONG).show();
+			}
+		});
+        
     }
 }
