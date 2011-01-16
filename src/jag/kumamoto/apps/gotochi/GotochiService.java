bad40d3ec9eab1d4ba42714dee2bb6141e6a4389
aharisu <foo.yobina@gmail.com>
2011-01-17 02:54:17 +0900

    現在位置判定クラスを追加
    ご当地サービスから現在位置判定クラスを使用するように修正
    現在位置判定のために、都道府県コードの内容を修正

--- src/jag/kumamoto/apps/gotochi/GotochiService.java
+++ src/jag/kumamoto/apps/gotochi/GotochiService.java
@@ -3,6 +3,7 @@ package jag.kumamoto.apps.gotochi;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.os.RemoteException;
 
@@ -13,8 +14,23 @@ import android.os.RemoteException;
  */
 public class GotochiService extends Service{
 	
+	private static final String PREFERENCES_NAME = "gotochi-pref";
+	private static final String LAST_KNOWN_LOCATION = "last-known-location";
+	
 	private boolean mIsRunning = false;
 	private int mActivityCount = 0;
+	private LocalLocationManager mLocalLocationManager;
+	private final LocalLocationManager.OnLocalLocationChangeListener mLocalLocationListener = 
+		new LocalLocationManager.OnLocalLocationChangeListener() {
+		
+		@Override public void onLocalLocationChange(PrefecturesCode cur, PrefecturesCode prev) {
+			//TODO インテントを投げる
+		}
+		
+		@Override public void onManagerException() {
+			checkDeviceCapabilities();
+		}
+	};
 	
 	private final IGotochiService.Stub mJudgmentLocationService = new IGotochiService.Stub() {
 
@@ -48,13 +64,31 @@ public class GotochiService extends Service{
 		}
 		
 		mIsRunning = true;
-		//TODO 位置判定モジュールのスタート
 		
-		
-		//TODO 初期位置(起動時の都道府県)をすぐに検索して
-		//一番最初のブロードキャストインテントを投げる
-		//or
-		//ぷリファレンスに保存しておいてそれを取得する
+		//位置判定モジュールのスタート
+		PrefecturesCode code = getLastKnownLocation();
+		mLocalLocationManager = new LocalLocationManager(this, code, mLocalLocationListener);
+		mLocalLocationManager.startJudgeLocation();
+	}
+	
+	private PrefecturesCode getLastKnownLocation() {
+		SharedPreferences pref = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+		int code =  pref.getInt(LAST_KNOWN_LOCATION, -1);
+		if(code != -1) {
+			return PrefecturesCode.values()[code - 1];
+		}
+		return null;
+	}
+	
+	private void putLastKnownLocation(PrefecturesCode code) {
+		if(code != null) {
+			SharedPreferences pref = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+			SharedPreferences.Editor editor = pref.edit();
+			
+			editor.putInt(LAST_KNOWN_LOCATION, code.code);
+			
+			editor.commit();
+		}
 	}
 	
 	private void onPause() {
@@ -71,7 +105,9 @@ public class GotochiService extends Service{
 		}
 		
 		mIsRunning = true;
-		//TODO 位置判定モジュールの再始動
+		
+		//位置判定モジュールの再始動
+		mLocalLocationManager.restartJudgeLocation();
 	}
 	
 	@Override public void onDestroy() {
@@ -80,10 +116,12 @@ public class GotochiService extends Service{
 		mIsRunning = false;
 		
 		stopLocationModule();
+		putLastKnownLocation(mLocalLocationManager.getCurrentPrefecturesCode());
+		mLocalLocationManager = null;
 	}
 	
 	private void stopLocationModule() {
-		//TODO 位置判定モジュールの終了
+		mLocalLocationManager.stopJudgeLocation();
 	}
 	
 	private boolean checkDeviceCapabilities() {
---
+++ src/jag/kumamoto/apps/gotochi/LocalLocationManager.java
@@ -0,0 +1,134 @@
+package jag.kumamoto.apps.gotochi;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public final class LocalLocationManager {
+	public static interface OnLocalLocationChangeListener {
+		public void onLocalLocationChange(PrefecturesCode cur, PrefecturesCode prev);
+		public void onManagerException();
+	}
+
+	private PrefecturesCode mCurrentLocation;
+	private final Context mContext;
+	private boolean mJudgeStart = false;
+	private final Geocoder mGeocoder;
+	private final OnLocalLocationChangeListener mCallback;
+	
+	public LocalLocationManager(Context context, OnLocalLocationChangeListener listener) {
+		this(context, null, listener);
+	}
+	
+	public LocalLocationManager(Context context, PrefecturesCode curLocation, 
+			OnLocalLocationChangeListener listener) {
+		
+		mCurrentLocation = curLocation;
+		mContext = context;
+		mGeocoder = new Geocoder(context, Locale.JAPAN);
+		mCallback = listener;
+	}
+	
+	public PrefecturesCode getCurrentPrefecturesCode() {
+		return mCurrentLocation;
+	}
+		private final LocationListener mLocationListener = new LocationListener() {
+		
+		@Override public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
+		
+		@Override public void onProviderEnabled(String provider) {
+		}
+		
+		@Override public void onProviderDisabled(String provider) {
+			if(provider.equals(LocationManager.GPS_PROVIDER)) {
+				mCallback.onManagerException();
+			}
+		}
+		
+		@Override public void onLocationChanged(Location location) {
+			try {
+				String adminArea = pointToAdminArea(location.getLatitude(), location.getLongitude());
+				if(adminArea != null) {
+					PrefecturesCode code = adminAreaToPrefecturesCode(adminArea);
+					if(code != null && mCurrentLocation != code) {
+						PrefecturesCode prev = mCurrentLocation;
+						mCurrentLocation = code;
+						
+						mCallback.onLocalLocationChange(mCurrentLocation, prev);
+					}
+				}
+			}catch(IOException e) {
+				mCallback.onManagerException();
+			}
+		}
+	};
+	
+	private String pointToAdminArea(double latitude, double longtitude) throws IOException{
+		
+		for(Address addr : mGeocoder.getFromLocation(latitude, longtitude, 5)) {
+			String adminArea = addr.getAdminArea();
+			if(adminArea != null) {
+				return adminArea;
+			}
+		}
+		
+		return null;
+	}
+	
+	private PrefecturesCode adminAreaToPrefecturesCode(String adminArea) {
+		if(mCurrentLocation != null && mCurrentLocation.name.equals(adminArea)) {
+			return mCurrentLocation;
+		}
+		
+		for(PrefecturesCode code : PrefecturesCode.values()) {
+			if(code.name.equals(adminArea)) {
+				return code;
+			}
+		}
+		
+		//例外を投げるようにするか？
+		return null;
+	}
+	
+	public void startJudgeLocation() {
+		setupLocationListener();
+		
+		LocationManager lm = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE); 
+		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		mLocationListener.onLocationChanged(location);
+	}
+	
+	public void restartJudgeLocation() {
+		setupLocationListener();
+	}
+	
+	private void setupLocationListener() {
+		if(!mJudgeStart) {
+			mJudgeStart = true;
+			
+			LocationManager lm = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE); 
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+					0,//TODO 数値を適切な値に修正
+					0,//TODO 数値を適切な値に修正
+					mLocationListener);
+		}
+	}
+	
+	public void stopJudgeLocation() {
+		if(mJudgeStart) {
+			LocationManager lm = (LocationManager)mContext.getSystemService(Context.LOCATION_SERVICE); 
+			lm.removeUpdates(mLocationListener);
+			
+			mJudgeStart = false;
+		}
+	}
+	
+}
