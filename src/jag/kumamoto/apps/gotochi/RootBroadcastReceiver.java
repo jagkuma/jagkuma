a2c64ddf7b16cbe9afd201a97f7e6b9137595cb6
aharisu <foo.yobina@gmail.com>
2011-01-28 07:01:34 +0900

    デバッグのため、県を移動したときにログを出力とトーストを表示する機能を追加。
    これはあくまでデバッグ目的であり、今後この機能は削除する予定。
    ログは、各デバイスのSDカードディレクトリ以下、/gotochi/log/change_location.logに出力される。

--- src/jag/kumamoto/apps/gotochi/RootBroadcastReceiver.java
+++ src/jag/kumamoto/apps/gotochi/RootBroadcastReceiver.java
@@ -1,10 +1,23 @@
 package jag.kumamoto.apps.gotochi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 public class RootBroadcastReceiver extends BroadcastReceiver{
 	
@@ -38,6 +51,77 @@ public class RootBroadcastReceiver extends BroadcastReceiver{
 			PrefecturesCode before = intent.hasExtra(BEFORE_LOCATION) ? 
 					(PrefecturesCode)intent.getSerializableExtra(BEFORE_LOCATION) :
 					null;
+					
+					
+			before = PrefecturesCode.Fukuoka;
+			if(cur != null && before != null) {
+				
+				//logとしてど時刻、fromの県、toの県を出力する
+				FileWriter fw = null;
+				 try {
+					 File file = new File(new StringBuilder()
+							.append(Environment.getExternalStorageDirectory())
+							.append(File.separator)
+							.append("gotochi")
+							.append(File.separator)
+							.append("log")
+							.append(File.separator)
+							.toString());
+					 if(!file.exists()) {
+						 file.mkdirs();
+					 }
+					 
+					fw = new FileWriter(new StringBuilder()
+							.append(Environment.getExternalStorageDirectory())
+							.append(File.separator)
+							.append("gotochi")
+							.append(File.separator)
+							.append("log")
+							.append(File.separator)
+							.append("change_location.log")
+							.toString(),
+						true);
+					
+					final Calendar calendar = Calendar.getInstance(Locale.JAPAN);
+					
+					Date data = new Date(
+							calendar.get(Calendar.YEAR) - 1900, 
+							calendar.get(Calendar.MONTH), 
+							calendar.get(Calendar.DAY_OF_MONTH), 
+							calendar.get(Calendar.HOUR_OF_DAY), 
+							calendar.get(Calendar.MINUTE), 
+							calendar.get(Calendar.SECOND));
+					SimpleDateFormat format = new SimpleDateFormat("EEE, dd MMM yyy HH:mm:ss", Locale.ENGLISH);
+					
+					fw.write(new StringBuffer()
+						.append(format.format(data)).append(",")
+						.append(before.toString()).append(",")
+						.append(cur.toString()).append("\n").toString());
+					
+					
+					//トーストもついでに表示しておく
+					Toast.makeText(context, new StringBuilder()
+								.append("change location ")
+								.append("from ").append(before.toString())
+								.append(" to ").append(cur.toString())
+								.toString(),
+							Toast.LENGTH_LONG).show();
+					
+					
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} finally {
+					if(fw != null) {
+						try {
+							fw.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
 					
 			Intent gotochiIntent = new Intent();
 			StringBuilder builder = new StringBuilder()
